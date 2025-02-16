0f63c6888de27e0993cd10d99e1a96685e74149c
nop <noplab90@gmail.com>
2025-02-16 11:30:43 +0000

    update train eval

--- robocup_v1/robocup_eval.py
+++ robocup_v1/robocup_eval.py
@@ -1,12 +1,65 @@
+from typing import Tuple
 import genesis as gs
-
 import numpy as np
-from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
+import torch
+from sb3_contrib.common.recurrent.policies import RecurrentActorCriticPolicy
+from sb3_contrib.common.recurrent.type_aliases import RNNStates
 
 from robocup_env import RoboCupEnv, get_env_cfg
 
 
+class OnnxableSB3Policy(torch.nn.Module):
+    def __init__(self, policy: RecurrentActorCriticPolicy):
+        super().__init__()
+        self.policy = policy
+
+    def forward(
+        self,
+        observation: torch.Tensor,
+        pi: torch.Tensor,
+        vf: torch.Tensor,
+        episode_start: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.policy(
+            observation,
+            RNNStates(
+                pi=pi,
+                vf=vf,
+            ),
+            episode_start,
+            deterministic=True,
+        )
+
+
 def main():
+    # Load the model
+    model = RecurrentPPO.load(path="checkpoints/ppo_robocup_step_9175040", device="cpu")
+
+    observation_size = model.observation_space.shape
+    dummy_observation = np.zeros((1, *observation_size), dtype=np.float32)
+    dummy_episode_starts = np.ones((1,), dtype=bool)
+
+    _, dummy_lstm_states = model.predict(
+        observation=dummy_observation,
+        state=None,
+        episode_start=dummy_episode_starts,
+        deterministic=True,
+    )
+
+    print(f"dummy_lstm_states: {dummy_lstm_states[0]}")
+
+    torch.onnx.export(
+        model=OnnxableSB3Policy(model.policy),
+        args=(
+            torch.randn((1, *observation_size), dtype=torch.float32),
+            torch.from_numpy(np.array([dummy_lstm_states[0], dummy_lstm_states[1]])),
+            torch.from_numpy(np.array([dummy_lstm_states[0], dummy_lstm_states[1]])),
+            torch.ones((1,), dtype=torch.float32),
+        ),
+        f="checkpoints/robocup.onnx",
+    )
+
     gs.init()
 
     env_cfg = get_env_cfg()
@@ -18,13 +71,20 @@ def main():
         show_viewer=True,
     )
 
-    # Load the model
-    model = PPO.load(path="checkpoints/ppo_robocup")
+    lstm_states = None
+    episode_starts = np.ones((1,), dtype=bool)
 
     obs = env.reset()
     for _ in range(1000):
-        actions, _ = model.predict(observation=obs[0], deterministic=True)
+        actions, lstm_states = model.predict(
+            observation=obs[0],
+            state=lstm_states,
+            episode_start=episode_starts,
+            deterministic=True,
+        )
         obs, reward, done, _ = env.step(actions=np.array([actions]))
+
+        episode_starts = done
 
         print(f"actions: {actions}")
         print(f"obs: {obs[0]}")
--- robocup_v1/robocup_train.py
+++ robocup_v1/robocup_train.py
@@ -1,6 +1,6 @@
 import genesis as gs
 
-from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.callbacks import BaseCallback
 
 from robocup_env import RoboCupEnv, get_env_cfg
@@ -9,7 +9,7 @@ from robocup_env import RoboCupEnv, get_env_cfg
 class TensorboardCallback(BaseCallback):
     def __init__(self, verbose=0):
         super(TensorboardCallback, self).__init__(verbose)
-        self._log_freq = 10
+        self._log_freq = 50
 
     def _on_step(self) -> bool:
         if self.n_calls % self._log_freq == 0:
@@ -18,6 +18,18 @@ class TensorboardCallback(BaseCallback):
             rewards_mean = sum(rewards) / len(rewards)
 
             self.logger.record("episode rewards_mean", rewards_mean)
+
+        return True
+
+
+class ModelCheckpointCallback(BaseCallback):
+    def __init__(self, verbose=0):
+        super(ModelCheckpointCallback, self).__init__(verbose)
+        self._log_freq = 64
+
+    def _on_step(self) -> bool:
+        if self.n_calls % self._log_freq == 0:
+            self.model.save("checkpoints/ppo_robocup_step_" + str(self.num_timesteps))
 
         return True
 
@@ -34,15 +46,21 @@ def main():
         show_viewer=False,
     )
 
-    model = PPO(
-        "MlpPolicy",
+    policy_kwargs = dict(net_arch=dict(pi=[128, 128, 128], vf=[128, 128, 128]))
+    model = RecurrentPPO(
+        "MlpLstmPolicy",
         env,
-        n_steps=512,
-        batch_size=8192,
+        n_steps=128,
+        batch_size=128 * 512,
         verbose=1,
         device="cuda",
         tensorboard_log="logs/robocup",
-    ).learn(total_timesteps=20000000, callback=TensorboardCallback(), progress_bar=True)
+        policy_kwargs=policy_kwargs,
+    ).learn(
+        total_timesteps=100000000,
+        callback=[TensorboardCallback(), ModelCheckpointCallback()],
+        progress_bar=True,
+    )
 
     # Save the model
     model.save("checkpoints/ppo_robocup")
